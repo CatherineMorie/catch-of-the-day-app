dccf421fdd4c352414b131500e79836ab775eae9
Catherine Morie <catherine.morie@wustl.edu>
2020-06-04 08:25:33 -0500

    change page URL with react-router.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop form from submitting
+    event.preventDefault();
+    // 2. Get text from user input
+    const storeName = this.myInput.current.value;
+    // 3. Change page to /store/whatever-user-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
