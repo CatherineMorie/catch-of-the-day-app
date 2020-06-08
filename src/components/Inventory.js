a66176c7f1215e3c958e730feb9c8e1aab862f21
Catherine Morie <catherine.morie@wustl.edu>
2020-06-08 12:06:18 -0500

    create inventory form for each fish component

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,11 +1,15 @@
 import AddFishForm from './AddFishForm';
+import EditFishForm from './EditFishForm';
 import React from 'react';
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className="Inventory">
-        <h2>Inventory!!!</h2>
+        <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map((key) => (
+          <EditFishForm key={key} fish={this.props.fishes[key]} />
+        ))}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>
           Load Sample Fishes
